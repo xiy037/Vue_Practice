5ba704f559b47c1bf9b354e71faeb13064f2fdb2
Xiao Yu <xiao.susan.yu@gmail.com>
2020-03-09 19:44:07 +0800

    [Xiao]{feat}: add sum price and count computed attributes

--- router-practice/src/views/Cart.vue
+++ router-practice/src/views/Cart.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
+  <div class="cart-container">
     Shopping cart
     <CartSearch v-if="showSearch" @hide="hideFn">
-      <div slot="notes" slot-scope="search">
+      <div class="search-results" slot="notes" slot-scope="search">
         <span class="slot-notes">slot demo for "notes"</span>
         <div>{{search.item}}</div>
       </div>
     </CartSearch>
     <a-button v-else @click="showFn">Show Search</a-button>
-
-    <a-table :columns="cols" :dataSource="shoppingList" rowKey="id">
+    <div class="total-price">
+      <div>Total:</div>
+      <div>{{sumCount}} items,</div>
+      <div>{{sumPrice}} rmb</div>
+    </div>
+    <a-table :columns="cols" :dataSource="shoppingList" rowKey="id" :pagination="pagination">
+      <div
+        class="table-header"
+        slot="title"
+        slot-scope="currentPageData"
+      >
+      In this page, total price is {{sumCurrent(currentPageData)}} RMB 
+      by summing up {{currentPageData.length}} items.
+      </div>
       <span slot="action" slot-scope="record">
         <a-button v-on:click="deleteItem(record)">Delete</a-button>
       </span>
@@ -66,8 +78,8 @@ export default {
           imgUrl: "xxxxx",
           content: "denim pants",
           store: "women store",
-          count: 1,
-          price: 299
+          count: 2,
+          price: 500
         },
         {
           id: 3,
@@ -84,9 +96,39 @@ export default {
           store: "Uniqle",
           count: 1,
           price: 199
+        },
+        {
+          id: 5,
+          imgUrl: "xxxxx",
+          content: "graphical T-shirt",
+          store: "Uniqle",
+          count: 1,
+          price: 199
+        },
+        {
+          id: 6,
+          imgUrl: "xxxxx",
+          content: "graphical T-shirt",
+          store: "Muji",
+          count: 1,
+          price: 199
+        },
+        {
+          id: 7,
+          imgUrl: "xxxxx",
+          content: "bag",
+          store: "Muji",
+          count: 1,
+          price: 299
         }
       ],
-      showSearch: true
+      showSearch: true,
+      pagination: {
+        pageSize: 5,
+        hideOnSinglePage: true,
+        showTotal: (total, range) =>
+          `Show ${range[0]} ~ ${range[1]} Records in ${total}`
+      }
     };
   },
   methods: {
@@ -98,13 +140,53 @@ export default {
     },
     showFn() {
       this.showSearch = true;
+    },
+    sumCurrent(arr) {
+      return arr.reduce((prev, curr) => {
+        return (prev += curr.price);
+      }, 0);
+    }
+  },
+  computed: {
+    sumPrice() {
+      return this.shoppingList.reduce((prev, curr) => {
+        return (prev += curr.price);
+      }, 0);
+    },
+    sumCount() {
+      return this.shoppingList.reduce((prev, curr) => {
+        return (prev += curr.count);
+      }, 0);
     }
   }
 };
 </script>
 <style scoped lang="less">
+.cart-container {
+   margin: auto;
+   width: 1000px;
+}
 .slot-notes {
   color: white;
   background-color: #2c3e50;
+}
+.search-results {
+  border: 1px solid grey;
+}
+.table-header {
+  font-size: 15px;
+  font-weight: 500;
+  color: #143828;
+}
+.total-price {
+  padding: 10px 40px;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 20px;
+  font-weight: 600;
+  background-color: #3da978;
+  div {
+    padding: 0 10px;
+  }
 }
 </style>
